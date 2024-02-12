35f6dc27fbc9e1240dae2400b1e483dbf1a7494b
PeterEkwere <peterekwere@icloud.com>
2024-02-12 19:26:44 +0100

    update 10 for task 1 and task 0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -18,13 +18,8 @@ def filter_datum(fields: List[str], redaction: str,
         message (list):  a list string representing the log line
         seperator (str): string character separating all fields
     """
-    for field in fields:
-        regex = f"{field}=[^{separator}]*"
-        message = re.sub(regex, f"{field}={redaction}", message)
-    return message
-    # return re.sub(
-        # fr'((?:^|\{separator})({"|".join(fields)})=)[^;]*',
-        # fr'\1{redaction}', message)
+    pattern = r'({})([^{}]*)'.format("|".join(fields), separator)
+    return re.sub(pattern, fr'\1={redaction}', message)
 
 
 class RedactingFormatter(logging.Formatter):
@@ -35,9 +30,9 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields):
-        super(RedactingFormatter, self).__init__(self.FORMAT)
+    def __init__(self, fields: List[str]):
         self.fields = fields
+        super(RedactingFormatter, self).__init__(self.FORMAT)
 
     def format(self, record: logging.LogRecord) -> str:
         """ This method filters values in incoming log records
@@ -49,5 +44,8 @@ class RedactingFormatter(logging.Formatter):
             str: _description_
         """
         new_message = super().format(record)
-        return filter_datum(self.fields, self.REDACTION,
-                            new_message, self.SEPARATOR)
+        message = record.getMessage()
+        new_message = filter_datum(self.fields, self.REDACTION,
+                                   message, self.SEPARATOR)
+        record.msg = new_message
+        return super().format(record)
