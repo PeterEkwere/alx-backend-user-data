9bea93af0496b2f04d760d3c9facf3a973abac1b
PeterEkwere <peterekwere@icloud.com>
2024-02-13 14:40:42 +0100

    update for task 1

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -7,9 +7,9 @@ from api.v1.views import app_views
 
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized():
-  """ THis function aborts with a  401 
-  """
-  abort(401)
+    """ This function aborts with a 401
+    """
+    abort(401)
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
