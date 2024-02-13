7a456488cd77339bbe490db3fbbcbf800f4512db
PeterEkwere <peterekwere@icloud.com>
2024-02-13 14:32:08 +0100

    initial commit for task 1

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -5,6 +5,13 @@ from flask import jsonify, abort
 from api.v1.views import app_views
 
 
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized():
+  """ THis function aborts with a  401 
+  """
+  abort(401)
+
+
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
     """ GET /api/v1/status
