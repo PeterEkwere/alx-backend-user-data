b7ba601b1a45e6fb942ad9b89e2cb5eec48bc919
PeterEkwere <peterekwere@icloud.com>
2024-02-14 23:48:56 +0100

    initial commit for task 5

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -14,10 +14,30 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
+auth_type = os.getenv("AUTH_TYPE")
+none_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
 
-if auth:
+
+if auth_type == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
+
+
+@app.before_request
+def authenticate():
+    """ This method filters each request
+
+    Returns:
+        _type_: _description_
+    """
+    if auth:
+        if auth.require_auth(request.path, none_paths):
+            if auth.authorization_header(request):
+                abort(401)
+            if auth.current_user(request) is None:
+                abort(403)
+    return
+
 
 @app.errorhandler(403)
 def not_found_403(error):
--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -39,9 +39,9 @@ class Auth:
         """
         if request is None:
             return None
-        if "Authorization" not in request:
+        if "Authorization" not in request.headers:
             return None
-        return request['Authorization']
+        return request.headers.get('Authorization', None)
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
