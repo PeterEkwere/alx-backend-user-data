b54b5280ae799fad45274a677f55e6b6e39dbe8a
PeterEkwere <peterekwere@icloud.com>
2024-02-15 23:06:29 +0100

    initial commit for task 7 and 8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """
     This module will contain the auth class
-    Author: Peteer Ekwere
+    Author: Peter Ekwere
 """
 from flask import request
+import base64
 from typing import List, TypeVar
 from api.v1.auth.auth import Auth
 
@@ -14,3 +15,42 @@ class BasicAuth(Auth):
 
     def __init__(self) -> None:
         super().__init__()
+
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """ This Method  returns the Base64 part of the Authorization header
+
+        Args:
+            authorization_header (str): authorization header
+
+        Returns:
+            str:
+        """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        split = authorization_header.split()
+        if len(split) < 2:
+            return None
+        stat = split[0]
+        auth = split[1]
+
+        if stat != 'Basic':
+            return None
+        return auth
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """ This Method returns the decoded value of a Base64 string
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded_byte = base64.b64decode(base64_authorization_header)
+            decoded_str = decoded_byte.decode('utf-8')
+            return decoded_str
+        except BaseException:
+            return None
