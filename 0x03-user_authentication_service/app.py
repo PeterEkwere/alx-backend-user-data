22070aac678ae7d0cf2810a667013d1ac6327bdd
PeterEkwere <peterekwere@icloud.com>
2024-02-24 13:35:27 +0100

    initial commit for task 7

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -4,9 +4,10 @@
 """
 from os import getenv
 from flask import Flask, jsonify, abort, request
-
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/", methods=['GET'], strict_slashes=False)
@@ -14,6 +15,19 @@ def index():
     """ simple index route
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=['POST'], strict_slashes=False)
+def register_user():
+    """_summary_
+    """
+    user_email = request.form.get("email")
+    user_password = request.form.get("password")
+    try:
+        AUTH.register_user(user_email, user_password)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+    return jsonify({"email": f"{user_email}", "message": "user created"})
 
 
 if __name__ == "__main__":
