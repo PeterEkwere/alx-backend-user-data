2f3abec7f16931ebf56c106d618645c1ab801cd3
PeterEkwere <peterekwere@icloud.com>
2024-02-24 11:31:18 +0100

    initial commit for task 0, 1, and 2

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import NoResultFound, InvalidRequestError
+from user import Base, User
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """ This method takes an email and hashed_password
+        And returns a user object
+
+        Args:
+            email (str): This is a user's email
+            hashed_password (str): This is a user's hashed password
+
+        Returns:
+            User: THis is the created user object
+        """
+        new_user = User()
+        new_user.email = email
+        new_user.hashed_password = hashed_password
+        self._session.add(new_user)
+        self._session.commit()
+        return new_user
+
+    def find_user_by(self, **kwd: dict) -> User:
+        """ THis method finds users by keyworded arguments
+
+        Returns:
+            User: _description_
+        """
+        try:
+            user = self._session.query(User).filter_by(**kwd).one()
+        except NoResultFound:
+            raise NoResultFound()
+        except InvalidRequestError:
+            raise InvalidRequestError()
+        return user
