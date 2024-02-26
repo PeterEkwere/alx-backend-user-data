ec9fc9bce01832178f2faad4cfffb3b5fcaf33a1
PeterEkwere <peterekwere@icloud.com>
2024-02-26 23:36:16 +0100

    final commit for main

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -2,13 +2,25 @@
 """
 Main file
 """
-from auth import Auth
+import requests
+EMAIL = "Peterekwere007@gmail.com"
+PASSWD = "1122"
+NEW_PASSWD = "t4rt1fl3tt3"
 
-email = 'bob@bob.com'
-password = 'MyPwdOfBob'
-auth = Auth()
 
-auth.register_user(email, password)
+def register_user(email: str, password: str) -> None:
+    payload = {f'email={email}', f'password={password}'}
+    url = "localhost:5000/users"
+    requests.get(url, params=payload)
 
-print(auth.create_session(email))
-print(auth.create_session("unknown@email.com"))
+
+if __name__ == "__main__":
+    register_user(EMAIL, PASSWD)
+"""    log_in_wrong_password(EMAIL, NEW_PASSWD)
+    profile_unlogged()
+    session_id = log_in(EMAIL, PASSWD)
+    profile_logged(session_id)
+    log_out(session_id)
+    reset_token = reset_password_token(EMAIL)
+    update_password(EMAIL, reset_token, NEW_PASSWD)
+    log_in(EMAIL, NEW_PASSWD)"""
